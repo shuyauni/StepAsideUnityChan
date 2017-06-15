3cf90e7666a85807911dbf1ffe254cddb0161d54
shuya matsunobe <tonn5nura32@gmail.com>
2017-06-16 00:12:08 +0900

    lesson6課題
    基本課題と発展課題両方完了済みです。

---
+++ Assets/DestroyManager.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DestroyManager : MonoBehaviour {
+    private GameObject myCameraPos;
+
+    // Use this for initialization
+    void Start () {
+        this.myCameraPos = GameObject.Find("Main Camera");
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (this.myCameraPos.transform.position.z >= this.transform.position.z)
+        {
+            Destroy(this.gameObject);
+        }
+        
+	}
+}
